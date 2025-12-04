a583eb0940e61b8d13ca923027502418cb8a4bfb
Manosh Suresh <manoshsuresh@gmail.com>
2025-12-05 02:29:50 +0530

    update fairness score calculation

--- backend/src/utils/fairnessScore.js
+++ backend/src/utils/fairnessScore.js
@@ -4,8 +4,8 @@ function calculateFairnessScore(issues) {
   let score = 100;
 
   issues.forEach((issue) => {
-    if (issue.severity === "High") score -= 20;
-    else if (issue.severity === "Medium") score -= 10;
+    if (issue.severity === "High") score -= 10;
+    else if (issue.severity === "Medium") score -= 7;
     else if (issue.severity === "Low") score -= 5;
   });
 
